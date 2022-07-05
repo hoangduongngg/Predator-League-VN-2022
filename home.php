44e6564516d87dbd90612af94f0e5dfbe70566ab
Hoang Duong Nguyen <hoangduongnguyen.tech@gmail.com>
2022-07-05 22:44:06 +0700

    Home page demo

---
+++ home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Predator League VN 2022
+    </title>
+    <link rel="stylesheet" href="asset/css/home.css">
+    
+</head>
+<body>
+    <div class="overlay-navigation">
+        <nav role="navigation">
+          <ul>
+            <li><a href="#" data-content="The beginning">Home</a></li>
+            <li><a href="#" data-content="Curious?">About</a></li>
+            <li><a href="#" data-content="I got game">Skills</a></li>
+            <li><a href="#" data-content="Only the finest">Works</a></li>
+            <li><a href="#" data-content="Don't hesitate">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+      
+      <section class="home">
+          <a href="https://codepen.io/fluxus/pen/gPWxXJ" target="_blank">Click for CSS version</a>
+        <div class="open-overlay">
+          <span class="bar-top"></span>
+          <span class="bar-middle"></span>
+          <span class="bar-bottom"></span>
+        </div>
+      </section>
+      <script src="asset/js/home.js"></script>
+    </body>
+    </html>
