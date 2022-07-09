c7409d8ef81222baa300b821984671de5aa36d68
Hoang Duong Nguyen <hoangduongnguyen.tech@gmail.com>
2022-07-09 17:00:11 +0700

    upload

--- contact.php
+++ contact.php
@@ -11,7 +11,7 @@
     <!-- top -->
     <?php include "menu.html" ?>
     <a href=""></a>
-    <img src="asset/img/Contact.svg" alt="anh">
+    <img src="https://scontent.fhan14-1.fna.fbcdn.net/v/t39.30808-6/285009528_5152232561478959_5799268302049040459_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=e3f864&_nc_ohc=FFuVACOgeD0AX_xib4I&_nc_ht=scontent.fhan14-1.fna&oh=00_AT_p8L4BTxB7e-whlUERu5NZANu0SkkGcUd5oPCLKyL0WA&oe=62CB99FA" alt="anh">
     <!-- footer -->
    <?php include "footer.html" ?>
 </body>
--- home.php
+++ home.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>REDLINE MUSIC PARTY</title>
+    <title>PREDATOR LEAGUE VIETNAM 2022</title>
 </head>
 <body>
 
@@ -12,15 +12,17 @@
     <?php include "menu.html" ?>
 
     <!-- content -->
-    <img src="https://scontent.fhan14-2.fna.fbcdn.net/v/t39.30808-6/285745123_5166110960091119_5452591427897690229_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=e3f864&_nc_ohc=2CioGzMBOuUAX8cQbHY&_nc_ht=scontent.fhan14-2.fna&oh=00_AT9nEPcXnbwn3ilK5jJsNaf3PWU0Sk_n-uuH3MvAPUuCnQ&oe=62CB2673" alt="picture">
+    <img src="https://scontent.fhan14-1.fna.fbcdn.net/v/t39.30808-6/285009528_5152232561478959_5799268302049040459_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=e3f864&_nc_ohc=FFuVACOgeD0AX_xib4I&_nc_ht=scontent.fhan14-1.fna&oh=00_AT_p8L4BTxB7e-whlUERu5NZANu0SkkGcUd5oPCLKyL0WA&oe=62CB99FA" alt="picture">
     <?php include "button_ticket.html" ?>
     
-    <?php include "content.html" ?>
+    <?php 
+    include "content.html" 
+    ?>
     
     <?php 
     // include "layout_blog.html" 
     ?>
-    <img src="asset/img/Contact.svg" alt="anh">
+    
     <!-- footer -->
    <?php include "footer.html" ?>
 </body>
